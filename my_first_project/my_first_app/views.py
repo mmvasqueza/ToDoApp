f515c263d46bcfe1004ffc9cf11c2d18259903ea
Marcelo Vasquez <ing.marcelovasquez81@gmail.com>
2026-01-21 22:55:20 -0600

    Se incluyeron las migraciones dentro del proyecto

--- my_first_project/my_first_app/views.py
+++ my_first_project/my_first_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+# Es sumamente importante la identación en Python
+# esta definición de función debe ir sin espacios al inicio
+# La vista se comunica con la plantilla (template)
+# y le pasa datos a través del contexto (context)
+# La vista recibe una petición (request) y devuelve una respuesta (response)
 # Create your views here.
 def CarView(request):
     car_list = [
