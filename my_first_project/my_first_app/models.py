72433304bdcd5c36783844546c70c8f8b97a8896
Marcelo Vasquez <ing.marcelovasquez81@gmail.com>
2026-01-25 00:35:50 -0600

    Se realizan migraciones a la base de datos y se insertan registros utilizando sqlite3

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Car(models.Model):
     title = models.TextField(max_length=250)
+    year = models.TextField(max_length=4, null=True)
+    
+#La representación en cadena del objeto Car
+    def __str__(self):
+        return f"{self.title} - {self.year}"
